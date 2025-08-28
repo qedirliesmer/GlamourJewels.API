bcd46faac1088ea3fef641dd88261359868ff7cc
esmerqedirli <smrqdirli14@gmail.com>
2025-08-28 10:42:23 -0700

    created dbcontext

---
+++ src/Infrastructure/GlamourJewels.Persistence/Contexts/GlamourJewelsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GlamourJewels.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlamourJewels.Persistence.Contexts;
+
+public class GlamourJewelsDbContext:DbContext
+{
+    public GlamourJewelsDbContext(DbContextOptions<GlamourJewelsDbContext> options):base(options)
+    {  
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
